3a589394d07b0bd46863e69bdd06e3082b8e6fd3
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-05-01 14:02:08 +0200

    initial commit

---
+++ appinfo/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * ownCloud - vinecellar
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Christoph Wurst <christoph@winzerhof-wurst.at>
+ * @copyright Christoph Wurst 2016
+ */
+
+namespace OCA\VineCellar\AppInfo;
+
+use OCP\AppFramework\App;
+
+/**
+ * Additional autoloader registration, e.g. registering composer autoloaders
+ */
+// require_once __DIR__ . '/../vendor/autoload.php';
